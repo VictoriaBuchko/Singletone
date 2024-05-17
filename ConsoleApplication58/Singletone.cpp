f959ee8877b01059cbde04eef64f29d9a750fc90
SunValley <buckovika0@gmail.com>
2024-05-17 12:30:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication58/Singletone.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+
+class Boss {
+private:
+    static Boss* s;
+    int health;
+    Boss(int i) {
+        health = i;
+    }
+public:
+
+    static Boss* getReference() {
+        return s;
+    }
+    static void destroy() {
+
+        if (s != nullptr) {
+            cout << "Destroy" << endl;
+            delete s;
+            s = nullptr;
+        }
+    }
+    
+    int getHealth() {
+        return health;
+    }
+    
+    void setHealth(int i) {
+        health = i;
+        cout << "Boss health after hit: " << getHealth() << "\n\n";
+        if (health <= 0) {
+            cout << "Boss is defeated!" << endl;
+            destroy();
+        }
+    }
+};
+
+Boss* Boss::s = new Boss(20);
+int main() {
+   
+    Boss* p = Boss::getReference();
+    
+    cout << "Boss health: " << p->getHealth() << "\n\n";
+    while (p->getHealth() > 0) {
+        p->setHealth(p->getHealth() - 4);
+    }
+
+}
+
+
+
